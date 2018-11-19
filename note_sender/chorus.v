76badf508a6cc7d83ac7c070e7b9721624d60111
josephxpin <44883475+josephxpin@users.noreply.github.com>
2018-11-19 17:14:54 -0500

    note_sender completed audio_otuput midway complete

--- note_sender/chorus.v
+++ note_sender/chorus.v
@@ -1,6 +1,7 @@
 module chorus(
 				  input clk,
 						  eight_beat,
+						  load,
 						  
 				  output reg [4:0]exp_notes
 				  );
@@ -9,40 +10,42 @@ module chorus(
 	
 	always@(posedge clk)
 	begin
-		if (eight_beat == 1'b1)
-			t <= t+1;
-		else
-			t <= t;
-		endcase
+		if (load)
+			if (eight_beat == 1'b1)
+				t <= t+1;
+			else if (t == 5'd31)
+				t <= 5'd0;
+		else if (~load)
+			t <= 5'd0;
 	end
 	
 	always@(posedge clk)
 	begin
 	
 		case(t)
-			5'd1:
+			5'd0:
 				exp_notes <= 5'b00101;
-			5'd3:
+			5'd2:
 				exp_notes <= 5'b01010;
-			5'd5:
+			5'd4:
 				exp_notes <= 5'b10100;
-			5'd8:
+			5'd7:
 				exp_notes <= 5'b00101;
-			5'd10:
+			5'd9:
 				exp_notes <= 5'b01010;
-			5'd12:
+			5'd11:
 				exp_notes <= 5'b11000;
-			5'd13:
+			5'd12:
 				exp_notes <= 5'b10100;
-			5'd17:
+			5'd16:
 				exp_notes <= 5'b00101;
-			5'd19:
+			5'd18:
 				exp_notes <= 5'b01010;
-			5'd21:
+			5'd20:
 				exp_notes <= 5'b10100;
-			5'd24:
+			5'd23:
 				exp_notes <= 5'b01010;
-			5'd26:
+			5'd25:
 				exp_notes <= 5'b00101;
 				
 			default: exp_notes <= 5'b00000;
